ba9438aab9656c59a7652e42ad5e40e7074ee05d
Snowman <snowman718@yandex.com>
2020-01-15 12:30:01 +0900

    fix frist task

--- scrap.py
+++ scrap.py
@@ -25,10 +25,10 @@ def getList(urls):
             driver.get(url)
             time.sleep(4)
             if len(driver.find_elements_by_xpath("//div[@id='captchaContainer']")):
-                # WebDriverWait(driver, 30).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"iframe[name^='a-'][src^='https://www.google.com/recaptcha/api2/anchor?']")))
-                # WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='recaptcha-checkbox goog-inline-block recaptcha-checkbox-unchecked rc-anchor-checkbox']/div[@class='recaptcha-checkbox-border']"))).click()
-                # time.sleep(30)
-                # driver.switch_to.default_content()
+                WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"iframe[name^='a-'][src^='https://www.google.com/recaptcha/api2/anchor?']")))
+                WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='recaptcha-checkbox goog-inline-block recaptcha-checkbox-unchecked rc-anchor-checkbox']/div[@class='recaptcha-checkbox-border']"))).click()
+                driver.switch_to_default_content()
+                WebDriverWait(driver, 150).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"span.recaptcha-checkbox.goog-inline-block.recaptcha-checkbox-unchecked.rc-anchor-checkbox.recaptcha-checkbox-checked")))
                 time.sleep(70)
             else:
                 print('no capthcha')
